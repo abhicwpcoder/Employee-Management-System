783bae3d3caa6a562edf14aa462cfb98da7e0b72
Abhijeet Depale Patil <155878393+abhicwpcoder@users.noreply.github.com>
2026-05-30 19:29:06 +0530

    changes in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,8 +16,18 @@ const limiter = rateLimit({
   max: 100
 });
 
+app.use(
+  cors({
+    origin: [
+      "https://employee-management-system-kohl-six.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
+);
+app.options("*", cors());
 app.use(helmet());
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
