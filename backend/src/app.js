09daad0da4f462c85e6c5bff8d36f55d5134fa93
Abhijeet Depale Patil <155878393+abhicwpcoder@users.noreply.github.com>
2026-05-30 18:18:59 +0530

    added client_rul in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,8 +16,13 @@ const limiter = rateLimit({
   max: 100
 });
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(helmet());
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
