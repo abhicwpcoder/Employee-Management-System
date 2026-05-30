e4160d050c1d824e0c43428039d41afdde81c290
Abhijeet Depale Patil <155878393+abhicwpcoder@users.noreply.github.com>
2026-05-30 15:21:20 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION:", err);
+});
+
 import dotenv from 'dotenv';
 import { connectDB } from './src/config/database.js';
 import app from './src/app.js';
@@ -11,13 +15,3 @@ connectDB();
 app.listen(PORT, () => {
   console.log(`✓ Server running on port ${PORT}`);
 });
-
-
-
-// Handle graceful shutdown
-// process.on('SIGTERM', () => {
-//   console.log('SIGTERM signal received: closing HTTP server');
-//   server.close(() => {
-//     console.log('HTTP server closed');
-//   });
-// });
